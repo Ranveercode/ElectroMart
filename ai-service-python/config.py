baface35401e52ede80b6f98d826d370fc61d914
Ranveercode <ranveersrivastava.cs@gmail.com>
2026-07-12 17:14:24 +0530

    deployment changes

--- ai-service-python/config.py
+++ ai-service-python/config.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenv #type: ignore
 
 # Load environment variables from current working directory first, and then from microservice folder
 load_dotenv()
